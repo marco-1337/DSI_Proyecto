4275e497db164fa83cb2b95d556be378b64e488a
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2025-05-08 11:26:57 +0200

    tabs

--- Triangle Dripper/Assets/Scripts/TarjetaP6.cs
+++ Triangle Dripper/Assets/Scripts/TarjetaP6.cs
@@ -6,33 +6,33 @@ using UnityEngine.UIElements;
 public class TarjetaP6
 {
     VisualElement tarjetaRoot;
-    IndividuoP6 myIndividuo;
+    //IndividuoP6 myIndividuo;
 
     Label nombreLabel;
     Label apellidoLabel;
     VisualElement backgroundImage;
 
-    public TarjetaP6(VisualElement tarjetaRoot, IndividuoP6 individuo)
-    {
-        this.tarjetaRoot = tarjetaRoot;
-        myIndividuo = individuo;
+    //public TarjetaP6(VisualElement tarjetaRoot, IndividuoP6 individuo)
+    //{
+    //    this.tarjetaRoot = tarjetaRoot;
+    //    myIndividuo = individuo;
 
-        nombreLabel = tarjetaRoot.Q<Label>("NameLabel");
-        apellidoLabel = tarjetaRoot.Q<Label>("SurnameLabel");
-        backgroundImage = tarjetaRoot.Q<VisualElement>("CardImage");
-        tarjetaRoot.userData = myIndividuo;
+    //    nombreLabel = tarjetaRoot.Q<Label>("NameLabel");
+    //    apellidoLabel = tarjetaRoot.Q<Label>("SurnameLabel");
+    //    backgroundImage = tarjetaRoot.Q<VisualElement>("CardImage");
+    //    tarjetaRoot.userData = myIndividuo;
 
-        UpdateUI();
+    //    UpdateUI();
 
-        myIndividuo.Cambio += UpdateUI;
-    }
+    //    myIndividuo.Cambio += UpdateUI;
+    //}
 
-    void UpdateUI()
-    {
-        nombreLabel.text = myIndividuo.Nombre;
-        apellidoLabel.text = myIndividuo.Apellido;
+    //void UpdateUI()
+    //{
+    //    nombreLabel.text = myIndividuo.Nombre;
+    //    apellidoLabel.text = myIndividuo.Apellido;
         
-        if (myIndividuo.BackgroundTexture != null)
-            backgroundImage.style.backgroundImage = myIndividuo.BackgroundTexture;
-    }
+    //    if (myIndividuo.BackgroundTexture != null)
+    //        backgroundImage.style.backgroundImage = myIndividuo.BackgroundTexture;
+    //}
 }
